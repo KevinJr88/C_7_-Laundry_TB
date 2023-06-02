30358ced29783a72df80d7b916f03c54bf88b7e3
KevinJr88 <99953819+KevinJr88@users.noreply.github.com>
2023-06-02 23:45:46 +0700

    tambah connection

--- src/connection/DbConnection.java
+++ src/connection/DbConnection.java
@@ -3,11 +3,37 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package connection;
-
+import java.sql.Connection;
+import java.sql.DriverManager;
 /**
  *
  * @author julia
  */
 public class DbConnection {
+    public static Connection CON;
+    public static final String URL = "jdbc:mysql://";
+    public static final String PATH = "localhost:3306/tubes-pbo";
     
+    public Connection makeConnection(){
+        System.out.println("Opening database...");
+        try{
+            CON = DriverManager.getConnection(URL + PATH, "root", "");
+            System.out.println("Success !");
+        }catch(Exception e){
+            System.out.println("Error when opening database ...");
+            System.out.println(e);
+        }
+        return CON;
+    }
+    
+    public void closeConnection(){
+        System.out.println("Closing database...");
+        try{
+            CON.close();
+            System.out.println("Success !");
+        } catch (Exception e){
+            System.out.println("Error when closing database...");
+            System.out.println(e);
+        }
+    }
 }
